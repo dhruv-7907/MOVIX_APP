2a8da0d42020ad8fc8b92d66184e2f7816239066
dhruvpatel18182@gmail.com <dhruvpatel18182@gmail.com>
2024-07-16 09:52:03 +0530

    moviCard to movicard

--- src/modules/explore/explore.jsx
+++ src/modules/explore/explore.jsx
@@ -7,7 +7,7 @@ import Select from "react-select";
 import ContentWrapper from '../../shared/contentWrapper/contentWrapper'
 import *as Action from '../../Redux/ExplorPage/explor_Action'
 import Carousel from "../../shared/carousel/carousel";
-import MovieCard from "../../shared/movieCard/movieCard";
+import MovieCard from "../../shared/movieCard/moviecard";
 import Spinner from "../../shared/spinner/spinner";
 import { useDispatch } from "react-redux";
 import "./style.scss";
